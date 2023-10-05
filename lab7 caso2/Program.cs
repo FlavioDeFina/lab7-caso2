fde3d96d45b9cd9fe33e163d2ffdd9a3beb62d10
José Manuel <Jos Manuel@DESKTOP-L67G7N4>
2023-10-04 22:49:05 -0500

    Agregar archivos de proyecto.

---
+++ lab7 caso2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7_caso2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado = 0;
+            for (int i = 1; i <= 1000; i++) 
+            {
+                resultado = i * 3;
+                Console.WriteLine("Divisor de Alonso De Fina "+i+": "+resultado);
+            }
+            Console.ReadKey();
+        }
+    }
+}
